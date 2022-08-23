c5f1c78ed00a95b4b5b691cdfb5cf95160a6a48c
AndresCamiloChavez <andres.cz.chavez@gmail.com>
2022-08-23 17:20:55 -0500

    Firts Commit with Canvas

---
+++ lib/main_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:get/get.dart';
+
+import 'main.dart';
+
+class MainController extends GetxController{
+  Rx<List<DrawingArea?>> points = Rx([]);
+  RxDouble strokeWidth = 2.0.obs;
+  Rx<Color> selectedColor = Colors.black.obs;
+
+}
